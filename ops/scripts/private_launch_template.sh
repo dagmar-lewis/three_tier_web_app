0676a329e50fb1ff04e6877d894a6a07a9e5ca74
Dagmar Lewis <dagmarlewis8@gmail.com>
2025-08-27 01:25:45 -0500

    update launch template scripts

--- ops/scripts/private_launch_template.sh
+++ ops/scripts/private_launch_template.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
+sudo apt install unzip
+
+curl -fsSL https://bun.sh/install | bash
+
+echo 'export PATH="$HOME/.bun/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+cd home/ubuntu
+
+git clone https://github.com/dagmar-lewis/three_tier_web_app.git
+
+cd three_tier_web_app
+
+cd backend
+
+bun install
+
+bun run dev
 
--- ops/scripts/public_launch_template.sh
+++ ops/scripts/public_launch_template.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
+sudo apt install unzip
+
+curl -fsSL https://bun.sh/install | bash
+
+echo 'export PATH="$HOME/.bun/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+cd home/ubuntu
+
+git clone https://github.com/dagmar-lewis/three_tier_web_app.git
+
+cd three_tier_web_app
+
+cd frontend
+
+bun install
+
+bun run dev
 
 
